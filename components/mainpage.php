4e4419d3f1d667959134cef3ce6a4257668a1f1b
Suyog <109020086+suyogchougale3096@users.noreply.github.com>
2023-04-21 11:45:55 +0530

    21

--- components/mainpage.php
+++ components/mainpage.php
@@ -5,10 +5,10 @@
 <div class = "container-fluid pt-5 pb-5">
     <div class = "row">
         <div class = "col-md-6 text-center mb-2">
-            <a href = "http://localhost/mini-project/components/studloginmain.php" class = "btn btn-success pt-5 pb-5 ps-5 pe-5"><i class="fa-solid fa-user main"></i><br>Student</a>
+            <a href = "http://localhost/Grievence-System/components/studloginmain.php" class = "btn btn-success pt-5 pb-5 ps-5 pe-5"><i class="fa-solid fa-user main"></i><br>Student</a>
         </div>
         <div class = "col-md-6 text-center">
-            <a href = "http://localhost/mini-project/components/teacherloginmain.php" class = "btn btn-success pt-5 pb-5 ps-5 pe-5"><i class="fa-solid fa-user main"></i><br>Teacher</a>
+            <a href = "http://localhost/Grievence-System/components/teacherloginmain.php" class = "btn btn-success pt-5 pb-5 ps-5 pe-5"><i class="fa-solid fa-user main"></i><br>Teacher</a>
         </div>
     </div>
 </div>
--- components/teacherloginhead.php
+++ components/teacherloginhead.php
@@ -44,7 +44,7 @@
                     <li class="nav-item"><a href="http://localhost/prj1/FacultyLogin.php" class = "nav-link me-lg-3">Faculty Login</a></li> -->
                     <li class="nav-item"><a href="#" class = "nav-link me-lg-3 headerlink">About</a></li>
                     <li class="nav-item"><a href="#" class = "nav-link me-lg-3 headerlink">Service</a></li>
-                    <li class="nav-item"><a href="http://localhost/mini-project/components/mainpage.php" class = "nav-link me-lg-5 headerlink">Back</a></li>
+                    <li class="nav-item"><a href="http://localhost/Grievence-System/components/mainpage.php" class = "nav-link me-lg-5 headerlink">Back</a></li>
                 </ul>
             </div>
         </div>
